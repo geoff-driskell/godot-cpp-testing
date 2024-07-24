52cb9b8a599b3235e8114f76340b3f77a60e2c62
Geoff Driskell <geoff.driskell@gmail.com>
2024-07-24 00:03:24 -0400

    Working Tooling!

--- src/register_types.cpp
+++ src/register_types.cpp
@@ -1,4 +1,5 @@
 #include "register_types.h"
+#include "Kart.hpp"
 #include <gdextension_interface.h>
 #include <godot_cpp/core/class_db.hpp>
 #include <godot_cpp/core/defs.hpp>
@@ -11,7 +12,7 @@ void initialize_gdextension_types(ModuleInitializationLevel p_level)
 	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
 		return;
 	}
-	//ClassDB::register_class<YourClass>();
+	ClassDB::register_class<GDExample>();
 }
 
 void uninitialize_gdextension_types(ModuleInitializationLevel p_level) {
@@ -23,7 +24,7 @@ void uninitialize_gdextension_types(ModuleInitializationLevel p_level) {
 extern "C"
 {
 	// Initialization
-	GDExtensionBool GDE_EXPORT example_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization)
+	GDExtensionBool GDE_EXPORT gdtest_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization)
 	{
 		GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
 		init_obj.register_initializer(initialize_gdextension_types);
